520a195e7e07e0ba11a15efc5c1b3a30da984a4a
José Antonio Ruiz <joseantonio.ruiz@grupoonetec.com>
2019-02-14 16:31:49 +0100

    remove / from sw.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,12 @@
 var url = window.location.href;
 var swLocation = '/twittor/sw.js';
 
-console.log('url', url);
-
-
 if (navigator.serviceWorker) {
 
 
     if (url.includes('localhost')) {
         swLocation = '/sw.js';
     }
-    console.log(swLocation);
     navigator.serviceWorker.register(swLocation);
 }
 
