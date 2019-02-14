4cdc8c83f5af30b7aaa464b95e09b4a917a36e2d
José Antonio Ruiz <joseantonio.ruiz@grupoonetec.com>
2019-02-14 16:26:37 +0100

    .

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ if (navigator.serviceWorker) {
     if (url.includes('localhost')) {
         swLocation = '/sw.js';
     }
+    console.log(swLocation);
     navigator.serviceWorker.register(swLocation);
 }
 
